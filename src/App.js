f6a52c00e22e3b089396be8c7592bd040cc5e1e5
¨mago¨ <martingramajo08@gmail.com>
2022-07-08 21:01:43 -0300

    agrego consulta a la Api par productos y usuarios

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import NavReact from './components/NavReact';
@@ -8,11 +10,59 @@ import Login from './pages/Login';
 import Perfil from './pages/Perfil';
 import Productos from './pages/Productos';
 import Register from './pages/Register';
+import { leerDeLocalStorage } from './utils/localStorage';
 
 function App() {
+const [productos, setProductos] = useState([])
+const [user, setUser] = useState({})
+const [isLoading, setIsLoading] = useState(true)
+
+   // consulta Api para traer los memes
+   const getProductos = async () => {
+    try {
+      const response = await axios.get('http://localhost:4000/api/Productos');
+      console.log("file: App.js ~ line 22 ~ getProductos ~ response", response)
+      setProductos(response.data) // setteamos la info de nuestro usuario. 
+    } catch (error) {
+      console.error(error);
+    }
+
+  }
+
+  useEffect(() => {
+    getProductos()
+  }, [])
+
+
+  // Consulta Api para traer la info del usuario en el login. 
+  const requestUserData = async () => {
+    setIsLoading(true)
+    const tokenLocal = leerDeLocalStorage("token") || {};
+    try {
+      if (tokenLocal.token) {
+        const headers = { 'x-auth-token': tokenLocal.token };
+        const response = await axios.get('http://localhost:4000/api/auth', { headers });
+        setUser(response.data) 
+      }
+    } catch (error) {
+      console.error(error);
+      localStorage.removeItem('token'); 
+      window.location.href = '/'; 
+    }
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    requestUserData()
+  }, [])
+
+  // Condicional para autorizar al user a que pagina va a poder acceder. 
+  const isAdmin = user.role === 'admin'
+
+
   return (
     <BrowserRouter>
-      <NavReact />
+      <NavReact  user={user}/>
       <Routes>
         <Route
           path="/"
@@ -21,7 +71,7 @@ function App() {
         <Route
           path="/Productos"
           element={
-            <Productos />} />
+            <Productos productos={productos} />} />
         <Route
           path="/Productos/:productoId"
           element={
@@ -29,15 +79,16 @@ function App() {
         <Route
           path="/Login"
           element={
-            <Login />} />
+            <Login requestUserData={requestUserData} />} />
         <Route
           path="/Register"
           element={
             <Register />} />
-        <Route
+        {isAdmin && <Route
           path="/Admin"
           element={
-            <Admin />} />
+            <Admin getProductos={getProductos} productos={productos} setProductos={setProductos} />} />}
+        
         <Route
           path="/Perfil"
           element={
