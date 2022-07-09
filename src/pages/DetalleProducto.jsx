72743d7c25fbe8b426cc4f48601e4e413951c750
¨mago¨ <martingramajo08@gmail.com>
2022-07-08 21:03:41 -0300

    detalle del producto

--- src/pages/DetalleProducto.jsx
+++ src/pages/DetalleProducto.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+import { Card } from "react-bootstrap";
 
 export default function DetalleProducto() {
+  const navigate = useNavigate();
+  const { productoId } = useParams();
+  const [producto, setProducto] = useState({});
+
+  // consulta Api para traer los Productos
+  useEffect(() => {
+    const getProductos = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:4000/api/Productos/${productoId}`
+        );
+        setProducto(response.data);
+      } catch (error) {
+        console.error(error);
+        navigate("*");
+      }
+    };
+    getProductos();
+  }, []);
+
   return (
-    <div>DetalleProductos</div>
-  )
+    <div className="d-flex justify-content-center">
+      {" "}
+      <Card className="card-meme my-5" style={{ width: "24rem" }}>
+        <Card.Img className="w-100" variant="top" src={producto.imagen} />
+        <Card.Body>
+          <Card.Title>{producto.titulo}</Card.Title>
+          <Card.Text>{producto.descripcion}</Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
